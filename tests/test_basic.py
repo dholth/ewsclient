2cff4afa98438cb2301ed0878f0a9ecb74e9076d
Daniel Holth <dholth@fastmail.fm>
2011-09-22 00:22:37 -0400

    broken

---
+++ tests/test_basic.py
@@ -0,0 +1,18 @@
+import ewsclient
+import os
+import suds.client
+from suds.transport.https import WindowsHttpAuthenticated
+
+def test_basic():
+    domain = os.environ.get('EWS_DOMAIN')
+    username = os.environ.get('EWS_USER')
+    password = os.environ.get('EWS_PASS')
+
+    transport = WindowsHttpAuthenticated(username=username,
+            password=password)
+    import pudb; pudb.set_trace()
+    client = suds.client.Client("https://%s/EWS/Services.wsdl" % domain,
+            transport=transport,
+            plugins=[ewsclient.AddService()])
+
+    return client
