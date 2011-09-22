bea189b8a0b926643a487f32a74d851aadba4b8f
Daniel Holth <dholth@fastmail.fm>
2011-09-22 00:48:01 -0400

    fix path error

--- ewsclient/__init__.py
+++ ewsclient/__init__.py
@@ -11,14 +11,14 @@ class AddService(suds.plugin.DocumentPlugin):
     def loaded(self, ctx):
         """Add missing service."""
         urlprefix = urlparse.urlparse(ctx.url)
-        service_url = urlparse.urlunparse(urlprefix[:2] + ('Exchange.asmx', '', '', ''))
-        servicexml = '''  <wsdl:service name="ExchangeServices">
+        service_url = urlparse.urlunparse(urlprefix[:2] + ('/EWS/Exchange.asmx', '', '', ''))
+        servicexml = u'''  <wsdl:service name="ExchangeServices">
     <wsdl:port name="ExchangeServicePort" binding="tns:ExchangeServiceBinding">
       <soap:address location="%s"/>
     </wsdl:port>
   </wsdl:service>
 </wsdl:definitions>''' % service_url
-        ctx.document = ctx.document.replace('</wsdl:definitions>', servicexml)
+        ctx.document = ctx.document.replace('</wsdl:definitions>', servicexml.encode('utf-8'))
         return ctx
 
 # Otherwise suds will try, and often fail, to download this document.
